3581b0ffd1cbc03619650cc31c24e57c7b82df81
Chaithanya- Bangaru <chaithanya.bangaru@gmail.com>
2019-01-07 09:40:39 -0600

    Dockerfile example

---
+++ hello-docker-world/src/main/java/techdotbangaru/App.java
@@ -0,0 +1,13 @@
+package techdotbangaru;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Docker World!  [from www.bangaru.tech]" );
+    }
+}
